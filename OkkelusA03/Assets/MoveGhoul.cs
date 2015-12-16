fb79e2d90307dac26b4aad6b5255c50c7473b680
Sjoerd <sjoerdteunisse@outlook.com>
2015-12-16 12:35:56 +0100

    DIT IS HET DAN>......

---
+++ OkkelusA03/Assets/MoveGhoul.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGhoul : MonoBehaviour {
+
+    Animator anim;
+    NavMeshAgent nav;
+    Transform player;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);
+    }
+}
