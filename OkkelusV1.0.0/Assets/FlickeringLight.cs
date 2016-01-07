e6762e005fc2337a1268ea6a6b76aa7496af4719
klokkenman1 <kelvinsnepvangers@gmail.com>
2016-01-07 09:25:14 +0100

    joeys script

---
+++ OkkelusV1.0.0/Assets/FlickeringLight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+
+    float flickerSpeed;
+    float time;
+
+    public Light light;
+
+	void Start () 
+    {
+        flickerSpeed = 2f;
+        time = 0f;
+        light.intensity = 0f;
+	}
+	
+	void Update ()
+    {
+        time += Time.deltaTime;
+
+        if ((int)time % flickerSpeed == 0)
+        {
+            light.intensity = 8f;
+            flickerSpeed = Random.Range(1, 4);
+        }
+        else
+        {
+            light.intensity = 0f;
+        }
+	}
+}
