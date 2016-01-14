cc188b61012b73ef979919e290e7a79073696da9
jordibastiaansen <jordi.bastiaansen@hotmail.com>
2016-01-14 16:38:44 +0100

    tp functie added (WIP unstable)
    tp functie werkt nog niet maar moet die nog goed fixen als alle scene's
    uptodate zijn

---
+++ OkkelusV2.0.0/Assets/GotoNextLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GotoNextLevel : MonoBehaviour {
+
+    [SerializeField]
+    private CapsuleCollider Player;
+    private bool hit;
+	
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider == Player && !hit)
+        {
+            hit = true;
+            PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level") + 1);
+            Application.LoadLevel("Level" + PlayerPrefs.GetInt("Level"));
+        }
+    }
+}
