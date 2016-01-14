2b6799f79db754bb4a2803cc642cfbeb01dcfcf9
okkeh96 <okke_trommelen@live.nl>
2016-01-14 11:46:03 +0100

    Level 4 Actiemoment
    Bord text groep 2
    Colliders stoel
    Actiemoment

---
+++ OkkelusV2.0.0/Assets/Scripts/ActieTrigger.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActieTrigger : MonoBehaviour {
+
+    public GameObject floor;
+   
+    public GameObject floorCollider;
+    public Material[] lavaArray;
+   
+    private bool isLava;
+    private int index;
+    private int lavaSpeed;
+
+    MeshRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = floor.GetComponent<MeshRenderer>();
+        isLava = false;
+        index = 0;
+        lavaSpeed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isLava && lavaSpeed == 5)
+        {
+            Debug.Log(lavaSpeed.ToString());
+            rend.material = lavaArray[index];
+            lavaSpeed = 0;
+            index++;
+        }
+        
+        if (isLava)
+        {
+            lavaSpeed++;
+        }
+        if (index == 44)
+        {
+            index = 0;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+           // rend.material = lava;
+            isLava = true;
+        }
+    }
+
+    public bool IsLava()
+    {
+        return isLava;
+    }
+}
---
+++ OkkelusV2.0.0/Assets/Scripts/FloorScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorScript : MonoBehaviour {
+
+    public Material lava;
+    public ActieTrigger trigger;
+    private bool isLava;
+
+	// Use this for initialization
+	void Start () {
+        isLava = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (trigger.IsLava())
+        {
+            this.isLava = true;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(this.GetComponent<MeshRenderer>().material.ToString());
+
+        if (this.isLava)
+        {
+            Debug.Log("Swagyolo430Blaze");
+        }
+    }
+}
