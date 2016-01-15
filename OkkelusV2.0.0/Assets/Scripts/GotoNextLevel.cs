15c129a5be1600af26cba9fc121ab94f7aac01c1
jordibastiaansen <jordi.bastiaansen@hotmail.com>
2016-01-15 14:29:15 +0100

    recreate tp

--- OkkelusV2.0.0/Assets/Scripts/GotoNextLevel.cs
+++ OkkelusV2.0.0/Assets/Scripts/GotoNextLevel.cs
@@ -6,15 +6,25 @@ public class GotoNextLevel : MonoBehaviour {
     [SerializeField]
     private CapsuleCollider Player;
     private bool hit;
+    private AsyncOperation loadnew;
 	
     void OnCollisionEnter(Collision col)
     {
         if (col.collider == Player && !hit)
         {
             hit = true;
-            Application.UnloadLevel("Level" + PlayerPrefs.GetInt("Level"));
-            PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level") + 1);
-            Application.LoadLevel("Level" + PlayerPrefs.GetInt("Level"));
+           // Application.LoadLevelAsync
+            //Application.UnloadLevel("Level" + PlayerPrefs.GetInt("Level"));
+            PlayerPrefs.SetInt("Level", 2);//PlayerPrefs.GetInt("Level") + 1);
+            loadnew = Application.LoadLevelAsync("Level" + PlayerPrefs.GetInt("Level"));
+        }
+    }
+    void OnCollisionStay(Collision col)
+    {
+        if (hit)
+        {
+            Debug.Log(loadnew.progress);
+            
         }
     }
 }
